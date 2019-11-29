4dc52d898afdc8aa7b80e5cf178eaaf05eb57305
symonline <goldbars.ng@gmail.com>
2019-11-29 18:21:38 +0100

    included migration2

---
+++ command.py
@@ -0,0 +1,12 @@
+import click
+from flask.cli import with_appcontext
+
+from app import db
+from app.models import HoldersRight
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
+
+    
