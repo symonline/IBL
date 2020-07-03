d13d03180dc607df2636b96e8c245c5b3be4bb8f
Oshoke <goldbars.ng@gmal.com>
2020-07-03 17:48:43 +0100

    soem edits

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     DEBUG = os.environ.get('DEBUG')
     HOST = os.environ.get('HOST')
-    DATABASE_URL = os.environ.get('DATABASE_URL') or 'postgres://uc4v16g50tov06:p370bbdeae5f5eb7ff404e011d9496b165eec5e5dd32baf764e990c7b908168e0@ec2-54-164-183-51.compute-1.amazonaws.com:5432/db4lfpbrveq0mq'
+    DATABASE_URL = os.environ.get('DATABASE_URL') or 'postgres://aeolghqbxijwhs:3276aad73bb4ba5e8e4a8a3a4e40dc79ed7d93d85a7cd4c5e70742e3bac5ed86@ec2-46-51-190-87.eu-west-1.compute.amazonaws.com:5432/d7d90ifqq940ed'
